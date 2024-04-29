cd37eea19865a3335ce06022d791dc22315cd37a
Sage9705 <edem22kumahor@gmail.com>
2024-04-29 21:01:00 +0000

    Implement basic todo management with Redux

--- store/rootReducer.ts
+++ store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import todosReducer from './slices/todosSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  todos: todosReducer,
 });
 
 export default rootReducer;
---
+++ store/slices/todosSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
+interface TodosState {
+  items: Todo[];
+}
+
+const initialState: TodosState = {
+  items: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.items.push({
+        id: Date.now().toString(),
+        text: action.payload,
+        completed: false,
+      });
+    },
+    toggleTodo: (state, action: PayloadAction<string>) => {
+      const todo = state.items.find(item => item.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, removeTodo } = todosSlice.actions;
+export default todosSlice.reducer;
