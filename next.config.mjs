d56c2e8a0ff2c8e833a5fde6f19123d41b856574
Sage9705 <edem22kumahor@gmail.com>
2024-08-03 18:09:20 +0100

    fix: Update Next.js configuration for ES module compatibility in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
       ignoreBuildErrors: true,
     },
-  }
+  };
   
-  module.exports = nextConfig
+  export default nextConfig;
